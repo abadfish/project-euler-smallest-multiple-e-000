684e6221676faee9b0f5de6202ba59d203634986
abadfish <kirsten.ofarrell@gmail.com>
2015-12-13 19:18:16 -0500

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,20 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def limit(limit)
+    @limit = limit
+  end
+
+  def limit
+    @limit
+  end
+
+
+  def lcm
+    1.upto(@limit).reduce(:lcm)
+  end
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,5 @@
 # Implement your procedural solution here!
+
+def smallest_multiple(limit)
+  1.upto(limit).reduce(:lcm)
+end
